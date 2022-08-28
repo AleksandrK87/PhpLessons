be8f0edba77eaa36ee54ba12a0c4471476bec73a
AleksandrK87 <94989991+AleksandrK87@users.noreply.github.com>
2022-08-28 18:42:52 +0300

    PhpLessons.3.1

--- admin/index.php
+++ admin/index.php
@@ -1,8 +1,19 @@
 <?php
-require("configs/db.php");
+require($_SERVER['DOCUMENT_ROOT'] . "/configs/db.php");
 
-require("partials/header.php");
+require($_SERVER['DOCUMENT_ROOT'] . '/admin/partials/header.php');
 
+if (isset($_SESSION["user_id"]) && $_SESSION["user_id"] != null) {
+   $sql= "SELECT * FROM users WHERE id=" . $_SESSION["user_id"];
+   $result= mysqli_query($conn, $sql);
+   $user = $result->fetch_assoc();
+   
+   if($user['role'] != "admin") {
+      header("Location: /login.php");
+   } else {
+      header("Location: /login.php");
+   }
+}
 ?>
 
 
--- partials/header.php
+++ partials/header.php
@@ -26,9 +26,21 @@ session_start();
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet">
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js"></script>
   <link rel="stylesheet" href="assets/css/normalize.css">
-  
+
 
   <meta name="theme-color" content="#fafafa">
 </head>
 
 <body>
+
+
+  <?php
+  if (isset($_SESSION["user_id"]) && $_SESSION["user_id"] != null) {
+    echo '<a href="logout.php">Logout</a>';
+  } else {
+  ?>
+    <a href="login.php">Login</a>
+    <a href="register.php">Register</a>
+  <?php
+  }
+  ?>
