396c29e0d26dcd79590be6419167f32744602bad
AleksandrK87 <94989991+AleksandrK87@users.noreply.github.com>
2022-08-29 10:00:20 +0300

    PhpLessons3.5

--- partials/header.php
+++ partials/header.php
@@ -20,34 +20,31 @@ if ($is_session || $is_cookie) {
 }
 ?>
 
-<!doctype html>
-<html class="no-js" lang="">
+<!DOCTYPE html>
+<html lang="en">
 
 <head>
-  <meta charset="utf-8">
-  <title></title>
-  <meta name="description" content="">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
 
-  <meta property="og:title" content="">
-  <meta property="og:type" content="">
-  <meta property="og:url" content="">
-  <meta property="og:image" content="">
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
 
-  <link rel="manifest" href="site.webmanifest">
-  <link rel="apple-touch-icon" href="icon.png">
-  <!-- Place favicon.ico in the root directory -->
+    <title>SB Admin 2 - Dashboard</title>
 
-  <link rel="stylesheet" href="assets/css/main.css">
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet">
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js"></script>
-  <link rel="stylesheet" href="assets/css/normalize.css">
+    <!-- Custom fonts for this template-->
+    <link href="/admin/assets/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+    <link
+        href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
+        rel="stylesheet">
 
+    <!-- Custom styles for this template-->
+    <link href="/admin/assets/css/sb-admin-2.min.css" rel="stylesheet">
 
-  <meta name="theme-color" content="#fafafa">
 </head>
 
-<body>
+<body id="page-top">
 
 
   <?php
