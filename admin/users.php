cad04dc08139ec072804aa6085b2e8766948502a
AleksandrK87 <94989991+AleksandrK87@users.noreply.github.com>
2022-08-29 15:29:16 +0300

    PhpLessons3.8(Edit dont work)

--- admin/users.php
+++ admin/users.php
@@ -205,56 +205,27 @@ require($_SERVER['DOCUMENT_ROOT'] . '/admin/partials/header.php');
       <!-- Begin Page Content -->
       <div class="container-fluid">
 
-         <!-- Page Heading -->
-         <h1 class="h3 mb-2 text-gray-800">Tables</h1>
-         <p class="mb-4">DataTables is a third party plugin that is used to generate the demo table below.
-            For more information about DataTables, please visit the <a target="_blank" href="https://datatables.net">official DataTables documentation</a>.</p>
+         
+         <?php
+         if(empty($_GET)){
+         require($_SERVER['DOCUMENT_ROOT'] . '/admin/modules/users/all.php');
+         }else {
+         switch ($_GET['page']) {
+            case 'edit':
+               require($_SERVER['DOCUMENT_ROOT'] . '/admin/modules/users/edit.php');
 
-         <!-- DataTales Example -->
-         <div class="card shadow mb-4">
-            <div class="card-header py-3">
-               <h6 class="m-0 font-weight-bold text-primary">User list</h6>
-            </div>
-            <div class="card-body">
-               <div class="table-responsive">
-                  <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
-                     <thead>
-                        <tr>
-                           <th>Id</th>
-                           <th>Username</th>
-                           <th>Email</th>
-                           <th>Role</th>
-                           <th>Option</th>
-                        </tr>
-                     </thead>
-                     <tbody>
-                     <?php
-                        $sql = "SELECT * FROM users";
-                        $result = mysqli_query($conn, $sql);
-                        while($row = $result->fetch_assoc()) {
-                           ?>
-                           <tr>
-                           <td><?php echo $row['id'];?></td>
-                           <td><?php echo $row['username'];?></td>
-                           <td><?php echo $row['email'];?></td>
-                           <td><?php echo $row['role'];?></td>
-                           <td>
-                              <a href="#" class="btn btn-warning"><i class="fas fa-pen"></i>Edit</a>
-                              <a href="#" class="btn btn-danger"><i class="fas fa-trash-alt"></i>Delete</a>
-                           </td>
-                        </tr>
-
-                           <?php 
-
-                        }   
-                     ?>
-                        
-                        
-                     </tbody>
-                  </table>
-               </div>
-            </div>
-         </div>
+               break;
+            case 'edit':
+                  # code...
+               break;   
+            
+            default:
+               # code...
+               break;
+         }
+         }
+         ?>
+         
 
       </div>
       <!-- /.container-fluid -->
